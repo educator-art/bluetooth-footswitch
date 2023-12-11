3fe74a638921fa351c5ba3a79fd8cfadf9d09a9a
e <153011054+educator-art@users.noreply.github.com>
2023-12-11 16:01:18 +0900

    ロジックの見直し

--- main.ts
+++ main.ts
@@ -1,38 +1,44 @@
 input.onPinPressed(TouchPin.P0, function () {
     click_count += 1
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
 let RESPONSE_TIME = 0
+let time = 0
 let click_count = 0
 keyboard.startKeyboardService()
-let time = input.runningTime()
+let ClickStartFlg = true
 basic.forever(function () {
-    RESPONSE_TIME = input.runningTime() - time
-    if (RESPONSE_TIME > 2) {
-        if (click_count == 2) {
-            keyboard.sendString(keyboard.keys(keyboard._Key.left))
-            basic.showLeds(`
-                # # # # .
-                # . . . #
-                # . . . #
-                # . . . #
-                # # # # .
-                `)
-            basic.showLeds(`
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                `)
+    if (click_count == 1) {
+        if (ClickStartFlg) {
+            time = input.runningTime()
+            ClickStartFlg = false
         }
+    }
+    RESPONSE_TIME = (input.runningTime() - time) / 1000
+    if (RESPONSE_TIME > 1) {
         if (click_count == 1) {
             keyboard.sendString(keyboard.keys(keyboard._Key.right))
+            ClickStartFlg = true
+            click_count = 0
             basic.showLeds(`
+                . . # . .
+                . . . # .
                 # # # # #
-                # . . . .
-                # # # # #
-                . . . . #
-                # # # # #
+                . . . # .
+                . . # . .
                 `)
             basic.showLeds(`
                 . . . . .
@@ -42,23 +48,24 @@ basic.forever(function () {
                 . . . . .
                 `)
         }
-        click_count = 0
-        RESPONSE_TIME = 0
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
-        basic.showLeds(`
-            # . . . #
-            . # . # .
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
-        basic.pause(2000)
-        time = input.runningTime()
+        if (click_count == 2) {
+            keyboard.sendString(keyboard.keys(keyboard._Key.left))
+            ClickStartFlg = true
+            click_count = 0
+            basic.showLeds(`
+                . . # . .
+                . # . . .
+                # # # # #
+                . # . . .
+                . . # . .
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+        }
     }
 })
